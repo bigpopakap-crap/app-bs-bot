fc085e21bc3b88625b521641692678d9856f5d95
Kapil Easwar <kapil@squareup.com>
2019-06-08 07:57:13 -0700

    Cleanup app.use

--- server/index.ts
+++ server/index.ts
@@ -26,7 +26,7 @@ app.use(
 
 app.use(API_PATH, api);
 
-app.get("*", app.use(express.static("public")));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}.`);
