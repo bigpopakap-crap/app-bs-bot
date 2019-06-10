f120814e116e6c2f29237ebcef3702797e30a4e5
Kapil Easwar <kapil@squareup.com>
2019-06-09 20:28:47 -0700

    First pass implementing the word API

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import RestypedRouter from "restyped-express-async";
 import BsBotApi from "../shared/types/bs-bot-api-restyped";
-
+import HttpStatus from 'http-status-codes';
 // TODO figure out why absolute path imports aren't working here
-import { fillTemplate } from '../shared/bs';
-import { randomWordProvider} from "../shared/bs";
+import {fillTemplate, randomWordProvider} from '../shared/bs';
 import VOCABS, {VocabName} from '../shared/vocab';
 import randomItem from 'random-item';
+
+import WordStorage from './storage/words';
+
+const wordStorage = new WordStorage();
 
 const app = express();
 const router = RestypedRouter<BsBotApi>(app);
@@ -24,6 +27,48 @@ router.get('/vocabName', async request => {
   return {
     vocabNames: <Array<VocabName>> Object.keys(VocabName)
   }
+});
+
+/* ************************************************************************
+                            INTERACT WITH WORDS
+ ************************************************************************ */
+router.get('/word/:id', async request => {
+  return wordStorage.get(request.params.id);
+});
+
+router.put('/word/:id', async (request, response) =>  {
+  const id = request.params.id;
+  const updatedWord = request.body;
+
+  if (id !== updatedWord.id) {
+    response.status(HttpStatus.BAD_REQUEST).end();
+  } else {
+    wordStorage.update(updatedWord);
+  }
+});
+
+router.delete('/word/:id', async request => {
+  wordStorage.delete(request.params.id);
+});
+
+router.post('/word', async request => {
+  return wordStorage.insert(request.body);
+});
+
+router.get('/words', async request => {
+  return wordStorage.search(request.query);
+});
+
+router.post('/words', async request => {
+  return wordStorage.insertAll(request.body);
+});
+
+router.put('/words', async request => {
+  wordStorage.updateAll(request.body);
+});
+
+router.delete('/words', async request => {
+  wordStorage.deleteAll(request.query.ids);
 });
 
 // Lastly, handle any unknown requests with a 404
--- src/shared/types/bs-bot-api-restyped.d.ts
+++ src/shared/types/bs-bot-api-restyped.d.ts
@@ -1,6 +1,11 @@
 import {VocabName} from "../vocab";
+import {StorageRowId, StoredObject} from "./storage";
+import {WordMetadata, WordQuery} from "./word-metadata";
+import {WordClass} from "./words";
+import {Optional} from "../utils/optional";
 
 export default interface BsBotApi {
+
     /* ************************************************************************
                              RANDOMLY GENERATE B.S.
      ************************************************************************ */
@@ -29,5 +34,82 @@ export default interface BsBotApi {
     /* ************************************************************************
                             INTERACT WITH WORDS
      ************************************************************************ */
+    "/word/:id": {
+        /**
+         * Get a single word by ID
+         */
+        GET: {
+            params: {
+                id: StorageRowId
+            },
+            response: Optional<StoredObject<WordMetadata<WordClass>>>
+        },
 
+        /**
+         * Update (replace) a single existing word
+         */
+        PUT: {
+            params: {
+                id: StorageRowId
+            },
+            body: StoredObject<WordMetadata<WordClass>>,
+            // TODO respond with the updated value
+        },
+
+        /**
+         * Delete a single existing word
+         */
+        DELETE: {
+            params: {
+                id: StorageRowId
+            },
+            // TODO respond with something indicating whether it was deleted
+        },
+    },
+
+    "/word": {
+        /**
+         * Create a single word
+         */
+        POST: {
+            body: WordMetadata<WordClass>,
+            response: StoredObject<WordMetadata<WordClass>>
+        },
+    }
+
+    "/words": {
+        /**
+         * Get multiple words with filters/query
+         */
+        GET: {
+            query: WordQuery,
+            response: Array<StoredObject<WordMetadata<WordClass>>>
+        },
+
+        /**
+         * Create multiple words
+         */
+        POST: {
+            body: Array<WordMetadata<WordClass>>,
+            response: Array<StoredObject<WordMetadata<WordClass>>>
+        },
+
+        /**
+         * Update (replace) multiple existing words
+         */
+        PUT: {
+            body: Array<StoredObject<WordMetadata<WordClass>>>,
+            // TODO respond with the updated values
+        },
+
+        /**
+         * Delete multilpe existing words
+         */
+        DELETE: {
+            query: {
+                ids: Array<StorageRowId>,
+                // TODO respond with something indicating which ones were deleted
+            }
+        }
+    }
 }
